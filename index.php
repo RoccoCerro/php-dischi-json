e7cdd83ca4a3dbfa60801b8ad37d89c18b126ef5
RoccoCerro <rcerro02@gmail.com>
2024-05-08 14:21:12 +0200

    Add: reset css

--- index.php
+++ index.php
@@ -6,28 +6,31 @@
   <title>Document</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/axios@1.6.7/dist/axios.min.js"></script>
+  <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
 
   <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 
   <div id="app">
-    <div class="container">
-      <div class="row flex-wrap">
-        <div class="col-4">
-          <div v-for="disk in discs" class="card">
-            <div class="card-header">
-              
-            </div>
-            <div class="card-body">
-              <h3>{{disk.title}}</h3>
-              <h6>{{disk.author}}</h6>
-              <p>{{disk.year}}</p>
+    <main class="page-main">
+      <div class="container">
+        <div class="row">
+          <div v-for="disk in discs" class="col-4 text-center">
+            <div class="card">
+              <div class="card-header">
+                <img :src="disk.poster" alt="">
+              </div>
+              <div class="card-body">
+                <h3>{{disk.title}}</h3>
+                <h6>{{disk.author}}</h6>
+                <p>{{disk.year}}</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </main>
   </div>
 
   <script src="./js/app.js"></script>
