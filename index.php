f4a90e515fa8fafbe9e03b6b9654e370c5679386
RoccoCerro <rcerro02@gmail.com>
2024-05-08 16:21:39 +0200

    Add: modal with now components

--- index.php
+++ index.php
@@ -37,24 +37,9 @@
           </div>
         </div>
         <!-- Modal -->
-        <div v-if="isVisible" class="ms-modal fs-1 text-white">
-          <div class="container" @click="closeModal()">
-            <div class="row justify-content-center align-items-center">
-              <div class="col-auto">
-                <div class="card">
-                  <div @click="showModal(index)" class="card-header">
-                    <img :src="discs[currentIndex].poster" alt="">
-                  </div>
-                  <div class="card-body">
-                    <h4>{{discs[currentIndex].title}}</h4>
-                    <div>{{discs[currentIndex].author}}</div>
-                    <p>{{discs[currentIndex].year}}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <?php
+        include __DIR__ . '/partials/template/modal.php';
+        ?>
       </div>
     </main>
     <!-- Footer -->
--- partials/template/modal.php
+++ partials/template/modal.php
@@ -1,11 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body class="body-modal">
-
-</body>
-</html>
+<div v-if="isVisible" class="ms-modal fs-1 text-white">
+  <div class="container" @click="closeModal()">
+    <div class="row justify-content-center align-items-center">
+      <div class="col-auto">
+        <div class="card">
+          <div @click="showModal(index)" class="card-header">
+            <img :src="discs[currentIndex].poster" alt="">
+          </div>
+          <div class="card-body">
+            <h4>{{discs[currentIndex].title}}</h4>
+            <div>{{discs[currentIndex].author}}</div>
+            <p>{{discs[currentIndex].year}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
